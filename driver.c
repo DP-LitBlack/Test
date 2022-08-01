a163aaf245226ae0ac8dc59d96bbb76e4bba6d84
Peng Dou <1530506690@qq.com>
2022-07-31 21:34:52 -0400

    thread sync 2 finished

--- driver.c
+++ driver.c
@@ -12,7 +12,7 @@
 #include "udp_server.h"
 #include "data_logging.h"
   
-pthread_t tid[2];
+pthread_t tid[3];
 int counter;
 pthread_mutex_t lock;
 
@@ -23,6 +23,8 @@ struct sockaddr_in cliaddr;
 struct {
     sem_t init_finished;
     sem_t init_failed;
+    sem_t init_processing;
+    sem_t init_connected;
 } shared;
 // void* trythis(void* arg)
 // {
@@ -42,14 +44,19 @@ struct {
 //     return NULL;
 // }
 
+
+
+
 void *thread_init_udp(void* arg) {
     // if (pthread_mutex_init(&lock, NULL) != 0) {
     //     printf("\n mutex init has failed\n");
     //     return 1;
     // }
     sem_wait(&shared.init_failed);
+    sem_post(&shared.init_processing);
     init_udp();
     sem_post(&shared.init_finished);
+    sem_post(&shared.init_connected);
     // pthread_mutex_destroy(&lock);
     // pthread_detach(pthread_self());
     // pthread_exit(NULL);
@@ -92,28 +99,44 @@ void *thread_interrupt(void* arg) {
     return NULL;
 }
 
+void *thread_init_timeout(void* arg) {
+    int trigger2 = 5000;
+
+    sem_wait(&shared.init_processing);
+
+    printf("Searching for data visualizer...\n");
+
+    struct timespec ts;
+    int error;
+    error = clock_gettime(CLOCK_REALTIME, &ts);
+    if (error != 0) {
+        printf("Error: Get clock time failed\n");
+        return NULL;
+    }
+    ts.tv_sec += 3;
+    ts.tv_nsec = 0;
+
+    error = sem_timedwait(&shared.init_connected, &ts);
+
+    if (error != 0) {
+        printf("No data visualizer found\n");
+        sleep(1);
+        pthread_cancel(tid[0]);
+    }
+
+    sem_post(&shared.init_finished);
+    return NULL;
+}
+
 int main(void)
 {
     int error;
 
     sem_init(&shared.init_failed, 0, 1); 
     sem_init(&shared.init_finished, 0, 0); 
-
-    // if (pthread_mutex_init(&lock, NULL) != 0) {
-    //     printf("\n mutex init has failed\n");
-    //     return 1;
-    // }
+    sem_init(&shared.init_processing, 0, 0); 
+    sem_init(&shared.init_connected, 0, 0); 
   
-    // while (i < 2) {
-    //     error = pthread_create(&(tid[i]),
-    //                            NULL,
-    //                            &trythis, NULL);
-    //     if (error != 0)
-    //         printf("\nThread can't be created :[%s]",
-    //                strerror(error));
-    //     i++;
-    // }
-    // tid[2].argv_string = 
     error = pthread_create(&(tid[0]),
                             NULL,
                             &thread_init_udp, NULL);
@@ -128,13 +151,15 @@ int main(void)
         printf("\nThread 1 can't be created :[%s]",
                 strerror(error));
   
+    error = pthread_create(&(tid[2]),
+                            NULL,
+                            &thread_init_timeout, NULL);
+    if (error != 0)
+        printf("\nThread 2 can't be created :[%s]",
+                strerror(error));
 
     pthread_join(tid[0], NULL);
     pthread_join(tid[1], NULL);
-    // if (pthread_join(tid, &ret) != 0) {
-    //     pthread_join(tid[1], ret);
-    //     exit(3);
-    // }
-    // pthread_mutex_destroy(&lock);
-    return 0;
+    pthread_join(tid[2], NULL);
+    pthread_exit(NULL);
 }
--- udp_client.py
+++ udp_client.py
@@ -2,6 +2,7 @@ import socket
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.widgets import Button
 
 msgFromClient       = "Hello UDP Server"
 bytesToSend         = str.encode(msgFromClient)
@@ -11,13 +12,20 @@ bufferSize          = 1024
 # Create figure for plotting
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
+plt.subplots_adjust(bottom=0.5)
 xs = []
 ys = []
 
 ani = animation
+paused = False
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Adjustable parameters
+xlimit = 20
+ylimit = 20
+xlength = 100
 
 # # This function is called periodically from FuncAnimation
 def animate(i, xs, ys):
@@ -36,12 +44,13 @@ def animate(i, xs, ys):
         return
 
     # Limit x and y lists to 20 items
-    xs = xs[-20:]
-    ys = ys[-20:]
+    xs = xs[-xlimit:]
+    ys = ys[-ylimit:]
 
     # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
+    if(not paused): 
+        ax.clear()
+        ax.plot(xs, ys)
 
     # Format plot
     plt.xticks(rotation=45, ha='right')
@@ -49,8 +58,24 @@ def animate(i, xs, ys):
     plt.title('TMP102 Temperature over Time')
     plt.ylabel('Temperature (deg C)')
 
+def pause(event):
+    pause = True
+
+def resume(event):
+    pause = False
+
+
+
+# axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
+# axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
+# bnext = Button(axnext, 'Pause')
+# bnext.on_clicked(pause)
+# bprev = Button(axprev, 'Resume')
+# bprev.on_clicked(resume)
+
 # Set up plot to call animate() function periodically
 ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=100)
+
 plt.show()
 
 
